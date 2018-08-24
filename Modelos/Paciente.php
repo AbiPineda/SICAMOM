894119d4ae83160ceb4305db9cdd057d63ba8663
Abigail <pineverdi@gmail.com>
2018-08-24 11:27:27 -0600

    modificacion de BD

---
+++ Modelos/Paciente.php
@@ -0,0 +1,20 @@
+<?php
+class Paciente
+{
+	private $nombre;
+	private $apellido;
+	private $dui;
+	private $telefono; 
+	private $fechaNacimiento;
+	private $tipo;
+
+	function __construct() {
+        
+    }
+    
+
+
+
+}
+
+?>
--- Registros/registroPaciente.php
+++ Registros/registroPaciente.php
@@ -1,6 +1,8 @@
 <?php
 
-include('../Conexion/conexion.php');
+include('Conexion/conexion.php');
+
+//atributos para Registro de Paciente
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $dui = $_POST['dui'];
@@ -8,12 +10,21 @@ $telefono = $_POST['telefono'];
 $fechaNacimiento = $_POST['fecha'];
 $tipo = $_POST['tipo'];
 
+//atributos para Registro de Responsable
+
+$nombreR = $_POST['nombreRes'];
+$apellidoR = $_POST['apellidoRes'];
+$duiR = $_POST['duiRes'];
+$telefonoR = $_POST['telefonoRes'];
+
 $sql = "INSERT INTO t_paciente(pac_cnombre,pac_capellido,pac_cdui,pac_ctelefono,pac_ffecha_nac,pac_ctipo_consulta) VALUES('$nombre','$apellido','$dui','$telefono','$fechaNacimiento','$tipo')"; 
+
+$sql2 = "INSERT INTO t_responsable(res_cnombre,res_capellidos,res_cdui,res_ctelefono) VALUES('$nombreR','$apellidoR','$duiR','$telefonoR')"; 
 
 $conexion = conectar();
 $ejecutar= mysqli_query($conexion,$sql);
-
-if($ejecutar){
+$ejecutar2= mysqli_query($conexion,$sql2);
+if($ejecutar && $ejecutar2){
    
     echo "Exito";
 }else{
