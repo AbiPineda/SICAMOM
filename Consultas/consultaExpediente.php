d4de81fffa8e691b1b5eda29ca4990956e1ad18f
Abigail <pineverdi@gmail.com>
2018-08-30 16:01:43 -0600

    probando

--- Consultas/consultaExpediente.php
+++ Consultas/consultaExpediente.php
@@ -3,114 +3,298 @@
 include_once '../plantilla/cabecera.php';
 include_once '../plantilla/menu.php';
 include_once '../plantilla/menu_lateral.php';
+include_once '../Conexion/conexion.php';
 
 ?>
+
 <html lang="en" >
 
 <head>
   <meta charset="UTF-8">
-  <title>Modificar Expediente</title>
-  
-  
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
-<link rel='stylesheet' href='https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css'>
+  <title>Responsive & Accessible Data Table</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
 
-<link rel="stylesheet" href="../css/tablaExpediente.css">
+<meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
   
+      <style>
+      /* NOTE: The styles were added inline because Prefixfree needs access to your styles and they must be inlined if they are on local disk! */
+      * {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+*:before, *:after {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif;
+  color: #5e5d52;
+}
+
+a {
+  color: #337aa8;
+}
+a:hover, a:focus {
+  color: #4b8ab2;
+}
+
+.container {
+  margin: 5% 3%;
+}
+@media (min-width: 48em) {
+  .container {
+    margin: 2%;
+  }
+}
+@media (min-width: 75em) {
+  .container {
+    margin: 2em auto;
+    max-width: 75em;
+  }
+}
+
+.responsive-table {
+  width: 100%;
+  margin-bottom: 1.5em;
+}
+@media (min-width: 48em) {
+  .responsive-table {
+    font-size: .9em;
+  }
+}
+@media (min-width: 62em) {
+  .responsive-table {
+    font-size: 1em;
+  }
+}
+.responsive-table thead {
+  position: absolute;
+  clip: rect(1px 1px 1px 1px);
+  /* IE6, IE7 */
+  clip: rect(1px, 1px, 1px, 1px);
+  padding: 0;
+  border: 0;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+}
+@media (min-width: 48em) {
+  .responsive-table thead {
+    position: relative;
+    clip: auto;
+    height: auto;
+    width: auto;
+    overflow: auto;
+  }
+}
+.responsive-table thead th {
+  background-color: #1d96b2;
+  border: 1px solid #1d96b2;
+  font-weight: normal;
+  text-align: center;
+  color: white;
+}
+.responsive-table thead th:first-of-type {
+  text-align: left;
+}
+.responsive-table tbody,
+.responsive-table tr,
+.responsive-table th,
+.responsive-table td {
+  display: block;
+  padding: 0;
+  text-align: left;
+  white-space: normal;
+}
+@media (min-width: 48em) {
+  .responsive-table tr {
+    display: table-row;
+  }
+}
+.responsive-table th,
+.responsive-table td {
+  padding: .5em;
+  vertical-align: middle;
+}
+@media (min-width: 30em) {
+  .responsive-table th,
+  .responsive-table td {
+    padding: .75em .5em;
+  }
+}
+@media (min-width: 48em) {
+  .responsive-table th,
+  .responsive-table td {
+    display: table-cell;
+    padding: .5em;
+  }
+}
+@media (min-width: 62em) {
+  .responsive-table th,
+  .responsive-table td {
+    padding: .75em .5em;
+  }
+}
+@media (min-width: 75em) {
+  .responsive-table th,
+  .responsive-table td {
+    padding: .75em;
+  }
+}
+.responsive-table caption {
+  margin-bottom: 1em;
+  font-size: 1em;
+  font-weight: bold;
+  text-align: center;
+}
+@media (min-width: 48em) {
+  .responsive-table caption {
+    font-size: 1.5em;
+  }
+}
+.responsive-table tfoot {
+  font-size: .8em;
+  font-style: italic;
+}
+@media (min-width: 62em) {
+  .responsive-table tfoot {
+    font-size: .9em;
+  }
+}
+@media (min-width: 48em) {
+  .responsive-table tbody {
+    display: table-row-group;
+  }
+}
+.responsive-table tbody tr {
+  margin-bottom: 1em;
+  border: 2px solid #1d96b2;
+}
+@media (min-width: 48em) {
+  .responsive-table tbody tr {
+    display: table-row;
+    border-width: 1px;
+  }
+}
+.responsive-table tbody tr:last-of-type {
+  margin-bottom: 0;
+}
+@media (min-width: 48em) {
+  .responsive-table tbody tr:nth-of-type(even) {
+    background-color: rgba(94, 93, 82, 0.1);
+  }
+}
+.responsive-table tbody th[scope="row"] {
+  background-color: #1d96b2;
+  color: white;
+}
+@media (min-width: 48em) {
+  .responsive-table tbody th[scope="row"] {
+    background-color: transparent;
+    color: #5e5d52;
+    text-align: left;
+  }
+}
+.responsive-table tbody td {
+  text-align: right;
+}
+@media (min-width: 30em) {
+  .responsive-table tbody td {
+    border-bottom: 1px solid #1d96b2;
+  }
+}
+@media (min-width: 48em) {
+  .responsive-table tbody td {
+    text-align: center;
+  }
+}
+.responsive-table tbody td[data-type=currency] {
+  text-align: right;
+}
+.responsive-table tbody td[data-title]:before {
+  content: attr(data-title);
+  float: left;
+  font-size: .8em;
+  color: rgba(94, 93, 82, 0.75);
+}
+@media (min-width: 30em) {
+  .responsive-table tbody td[data-title]:before {
+    font-size: .9em;
+  }
+}
+@media (min-width: 48em) {
+  .responsive-table tbody td[data-title]:before {
+    content: none;
+  }
+}
+
+    </style>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
 </head>
 
 <body>
+    <div class="page-wrapper" style="height: 671px;">
+  <div class="container-fluid">
+      
+      <div class="col-md-8">
+  <table class="responsive-table">
+    
+    <thead>
+      <tr>
+        <th scope="col">Nombre</th>
+        <th scope="col">Apellido</th>
+        <th scope="col">DUI</th>
+        <th scope="col">Telefono</th>
+        <th scope="col">Fecha de Nacimiento</th>
+        <th scope="col">Accion</th>
+        
+        
+      </tr>
+    </thead>
+    <tfoot>
+     
+    </tfoot>
+    <tbody>
+        <?php
+        $sacar = mysqli_query($conexion, "SELECT*FROM t_paciente");
+            while ($fila = mysqli_fetch_array($sacar)) {
+                  $modificar=$fila['id_paciente']; 
+                 $ape=$fila['pac_capellidos'];  
+                 $nom=$fila['pac_cnombre'];  
+                 $dui=$fila['pac_cdui'];  
+                 $tel=$fila['pac_ctelefono'];  
+                 $fe=$fila['pac_ffecha_nac'];  
+            
+        ?>
+      <tr>
+        <th scope="row"><?php echo $nom;?></th>
+        <td data-title="Released"><?php echo $ape;?></td>
+        
+        <td data-title="Studio"><?php echo $dui;?></td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $tel;?></td>
+        <td data-title="Domestic Gross" data-type="currency"><?php echo $fe;?></td>
+        <td class="text-center fas fa-edit"><a href="../Consultas/modificarPaciente.php?ir=<?php echo $modificar; ?>" class="btn">Modificar</a></td>
 
-  
-  <h1>Modificar Expediente</h1>
+       <?php  }?>
+      
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+      </div>
 
-  <div class="table-container">
-    <div class="table-header">
-      <h2>Customer Table</h2>
-      <button class="add-another btn btn-success">Agregar Paciente</button>
-    </div>
-    <table class="customer-table table table-hover" style="width:200%">
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Dui</th>
-          <th>Telefono</th>
-          <th>Fecha de Nacimiento</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-
-      </tbody>
-    </table>
-  </div>
-
-  <div class="form-container">
-    <form name="customer-details" class="customer-details form-horizontal">
-      <fieldset>
-        <legend>Customer details</legend>
-        <div class="form-group">
-          <label for="txtName" class="control-label col-md-5">*Nombre:</label>
-          <div class="col-md-8">
-            <input type="text" id="txtName" name="name" placeholder="Enter Your Name Here" maxlength="50">
-            <div class="error-msg"></div>
-          </div>
-        </div>
-         <div class="form-group">
-          <label for="txtName" class="control-label col-md-5">*Apellido:</label>
-          <div class="col-md-8">
-            <input type="text" id="txtName" name="name" placeholder="Enter Your Name Here" maxlength="50">
-            <div class="error-msg"></div>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="rdoGender" class="control-label col-md-5">*Gender:</label>
-          <div class="col-md-8">
-            <label><input type="radio" class="rdo-gender" id="rdoGenderMale" name="gender" value="Male"><span>Male</span></label>
-            <label class="radio-gender"><input type="radio" name="gender" id="rdoGenderFemale" class="rdo-gender" value="Female"><span>Female</span></label>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="txtCountry" class="control-label col-md-5">*Country:</label>
-          <div class="col-md-8">
-            <input type="text" id="txtCountry" name="country" placeholder="Country Name" maxlength="50">
-            <div class="error-msg"></div>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="txtNumber" class="control-label col-md-5">*Phone Number:</label>
-          <div class="col-md-8">
-            <input type="text" id="txtNumber" name="phone" placeholder="Phone Number Here" maxlength="20">
-            <div class="error-msg"></div>
-          </div>
-        </div>
-        <button class="reset-button btn btn-danger">Reset</button>
-        <button class="add-button btn btn-success">Add</button>
-        <button class="update-button btn btn-primary">Update</button>
-      </fieldset>
-    </form>
-  </div>
-
-  <div class="message"></div>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-</body>
-  
-  
-
-    <script  src="js/index.js"></script>
-
-
-
-
+</div>
 </body>
 
 </html>
-
-
 
 <?php
     
