2cb35d14693693c97019745a05adcb71df6ff963
Kevin Montano <kevin_rey09@hotmail.com>
2018-09-04 06:32:07 -0600

    Dar baja
    Pantalla dar baja

---
+++ Consultas/darBaja.php
@@ -0,0 +1,317 @@
+<?php
+
+include_once '../plantilla/cabecera.php';
+include_once '../plantilla/menu.php';
+include_once '../plantilla/menu_lateral.php';
+include_once '../Conexion/conexion.php';
+
+?>
+
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Responsive & Accessible Data Table</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+
+<meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  
+      <style>
+      /* NOTE: The styles were added inline because Prefixfree needs access to your styles and they must be inlined if they are on local disk! */
+      * {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+*:before, *:after {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif;
+  color: black;
+}
+
+a {
+  color: black;
+}
+a:hover, a:focus {
+  color: #4b8ab2;
+}
+
+.container {
+  margin: 5% 3%;
+}
+@media (min-width: 48em) {
+  .container {
+    margin: 2%;
+  }
+}
+@media (min-width: 75em) {
+  .container {
+    margin: 2em auto;
+    max-width: 75em;
+  }
+}
+
+.responsive-table {
+  width: 100%;
+  margin-bottom: 1.5em;
+}
+@media (min-width: 48em) {
+  .responsive-table {
+    font-size: .9em;
+  }
+}
+@media (min-width: 62em) {
+  .responsive-table {
+    font-size: 1em;
+  }
+}
+.responsive-table thead {
+  position: absolute;
+  clip: rect(1px 1px 1px 1px);
+  /* IE6, IE7 */
+  clip: rect(1px, 1px, 1px, 1px);
+  padding: 0;
+  border: 0;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+}
+@media (min-width: 48em) {
+  .responsive-table thead {
+    position: relative;
+    clip: auto;
+    height: auto;
+    width: auto;
+    overflow: auto;
+  }
+}
+.responsive-table thead th {
+  background-color: #1d96b2;
+  border: 1px solid #1d96b2;
+  font-weight: normal;
+  text-align: center;
+  color: white;
+}
+.responsive-table thead th:first-of-type {
+  text-align: left;
+}
+.responsive-table tbody,
+.responsive-table tr,
+.responsive-table th,
+.responsive-table td {
+  display: block;
+  padding: 0;
+  text-align: left;
+  white-space: normal;
+}
+@media (min-width: 48em) {
+  .responsive-table tr {
+    display: table-row;
+  }
+}
+.responsive-table th,
+.responsive-table td {
+  padding: .5em;
+  vertical-align: middle;
+}
+@media (min-width: 30em) {
+  .responsive-table th,
+  .responsive-table td {
+    padding: .75em .5em;
+  }
+}
+@media (min-width: 48em) {
+  .responsive-table th,
+  .responsive-table td {
+    display: table-cell;
+    padding: .5em;
+  }
+}
+@media (min-width: 62em) {
+  .responsive-table th,
+  .responsive-table td {
+    padding: .75em .5em;
+  }
+}
+@media (min-width: 75em) {
+  .responsive-table th,
+  .responsive-table td {
+    padding: .75em;
+  }
+}
+.responsive-table caption {
+  margin-bottom: 1em;
+  font-size: 1em;
+  font-weight: bold;
+  text-align: center;
+}
+@media (min-width: 48em) {
+  .responsive-table caption {
+    font-size: 1.5em;
+  }
+}
+.responsive-table tfoot {
+  font-size: .8em;
+  font-style: italic;
+}
+@media (min-width: 62em) {
+  .responsive-table tfoot {
+    font-size: .9em;
+  }
+}
+@media (min-width: 48em) {
+  .responsive-table tbody {
+    display: table-row-group;
+  }
+}
+.responsive-table tbody tr {
+  margin-bottom: 1em;
+  border: 2px solid #1d96b2;
+}
+@media (min-width: 48em) {
+  .responsive-table tbody tr {
+    display: table-row;
+    border-width: 1px;
+  }
+}
+.responsive-table tbody tr:last-of-type {
+  margin-bottom: 0;
+}
+@media (min-width: 48em) {
+  .responsive-table tbody tr:nth-of-type(even) {
+    background-color: rgba(94, 93, 82, 0.1);
+  }
+}
+.responsive-table tbody th[scope="row"] {
+  background-color: #1d96b2;
+  color: white;
+}
+@media (min-width: 48em) {
+  .responsive-table tbody th[scope="row"] {
+    background-color: transparent;
+    color:black;
+    text-align: left;
+  }
+}
+.responsive-table tbody td {
+  text-align: right;
+}
+@media (min-width: 30em) {
+  .responsive-table tbody td {
+    border-bottom: 1px solid #1d96b2;
+  }
+}
+@media (min-width: 48em) {
+  .responsive-table tbody td {
+    text-align: center;
+  }
+}
+.responsive-table tbody td[data-type=currency] {
+  text-align: right;
+}
+.responsive-table tbody td[data-title]:before {
+  content: attr(data-title);
+  float: left;
+  font-size: .8em;
+  color: rgba(94, 93, 82, 0.75);
+}
+@media (min-width: 30em) {
+  .responsive-table tbody td[data-title]:before {
+    font-size: .9em;
+  }
+}
+@media (min-width: 48em) {
+  .responsive-table tbody td[data-title]:before {
+    content: none;
+  }
+}
+
+    </style>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
+</head>
+
+<body>
+    <div class="page-wrapper" style="height: 671px;">
+  <div class="container-fluid">
+     
+    <!-- Búsqueda UTILIZO EL JQUERY buscaresc.js que es el que hace el proceso interno de buscar
+    funciona junto con jquery de lo contrario nada colocas el id="filtar" que con ese nombre lo reconoce
+    el buscaresc.js para hacer el proceso que keres buscar ya sea por letras,numeros,dui, nit, loq sea
+    solo eso necesitas para que busque-->
+             <div class="wrap">
+                 <script src="../html/js/jquery.min.js" ></script>
+                 <script src="../html/js/buscaresc.js"></script>
+       <div class="search">
+          <input type="text" name="buscar" id="filtrar" class="searchTerm" placeholder="Que está buscando?">
+       </div>
+            </div>
+            <!--Fin Búsqueda-->
+      <div class="card" style="background: rgba(176, 176, 176,0.6)">
+      <div class="col-md-12">
+  <table class="responsive-table">
+    
+    <thead>
+      <tr>
+        <th scope="col">Nombre</th>
+        <th scope="col">Apellido</th>
+        <th scope="col">DUI</th>
+        <th scope="col">Telefono</th>
+        <th scope="col">Estado</th>
+        <!-- Donde irá el estado de la paciente, Inactiva o Activa-->
+        <th scope="col">Accion</th>
+        
+        
+      </tr>
+    </thead>
+    <tfoot>
+     
+    </tfoot>
+    <tbody class="buscar"> <!--mandas a llamar la clase del jquey para que lo aga automaticamente-->
+        <?php
+        $sacar = mysqli_query($conexion, "SELECT*FROM t_paciente");
+            while ($fila = mysqli_fetch_array($sacar)) {
+                  $modificar=$fila['id_paciente']; 
+                 $ape=$fila['pac_capellidos'];  
+                 $nom=$fila['pac_cnombre'];  
+                 $dui=$fila['pac_cdui'];  
+                 $tel=$fila['pac_ctelefono'];  
+                 $fe=$fila['estado'];  
+            
+        ?>
+      <tr>
+        <th scope="row"><?php echo $nom;?></th>
+        <td data-title="Released"><?php echo $ape;?></td>
+        
+        <td data-title="Studio"><?php echo $dui;?></td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $tel;?></td>
+        <td data-title="Domestic Gross" data-type="currency"><?php echo $fe;?></td>
+        <td class="text-center fas fa-arrow-circle-down"><a href="../Consultas/modificarPaciente.php?ir=<?php echo $modificar; ?>" class="btn">Dar Baja</a></td>
+
+       <?php  }?>
+      
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+      </div>
+    </div>
+</div>
+</body>
+
+</html>
+
+<?php
+    
+    include_once '../plantilla/pie.php';
+
+?>
