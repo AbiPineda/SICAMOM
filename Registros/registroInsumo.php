c0f9ac36e840bf0f3219891bbfe7a98acc3ac193
Abigail <pineverdi@gmail.com>
2018-09-20 16:21:38 -0600

    Generacion de codigo - Registro Insumo

--- Registros/registroInsumo.php
+++ Registros/registroInsumo.php
@@ -2,24 +2,35 @@
 include_once '../plantilla/cabecera.php';
 include_once '../plantilla/menu.php';
 include_once '../plantilla/menu_lateral.php';
+$codigo1= '';
 
 if (isset($_REQUEST['btnGuardar'])) {
     include_once '../Conexion/conexion.php';
 
+    $codigo = $_REQUEST['codigo'];
     $nombre = $_REQUEST['nombreCom'];
     $marca = $_REQUEST['marca'];
     $descripcion = $_REQUEST['descripcion'];
     $precio = $_REQUEST['precio'];
+    $caducidad = $_REQUEST['tipoCaducidad'];
     $presentacion = $_REQUEST['presentacion'];
+    if($caducidad == '0'){
     $fecha = date('Y-m-d', strtotime($_REQUEST['fecha']));
-    
+    }else{
+        $fecha ='0000-00-00';
+    }
   // $partes = explode('-', $fecha);
    // $_fecha = "{$partes[2]}-{$partes[1]}-{$partes[0]}";
     $esta=1;
+     //$numero = rand(100,1000);
+     //$codigo1 = (strtoupper((substr($nombre, 0, 3))) . $numero); 
+    // $anio = date("y");
+     
+               
     Conexion::abrir_conexion(); 
     $conexionx = Conexion::obtener_conexion();
     
-    mysqli_query($conexion,"INSERT INTO t_insumo(ins_cnombre_comercial,ins_cmarca,ins_cdescripcion,ins_cpresentacion,ins_dprecio,ins_ffecha_caducidad,estado) VALUES('$nombre','$marca','$descripcion','$presentacion','$precio','$fecha','$esta')"); 
+    mysqli_query($conexion,"INSERT INTO t_insumo(ins_cnombre_comercial,ins_cmarca,ins_cdescripcion,ins_cpresentacion,ins_dprecio,ins_ffecha_caducidad,estado,codigo) VALUES('$nombre','$marca','$descripcion','$presentacion','$precio','$fecha','$esta',' $codigo')"); 
 
     $insumo = mysqli_query($conexion, "SELECT*FROM t_insumo ORDER BY ins_codigo DESC LIMIT 1");
                                     while ($row = mysqli_fetch_array($insumo)) {
@@ -39,6 +50,8 @@ if (isset($_REQUEST['btnGuardar'])) {
                     location.href="modalInsumo.php";
                     
                 });</script>';
+    
+    
    
   //  $sentencia = $conexionx->prepare($sql);
     //$usuario_insertado = $sentencia->execute();
@@ -61,9 +74,16 @@ if (isset($_REQUEST['btnGuardar'])) {
                                <section>
 
                                      <div class="row mb-12">
-                                    <div class="col-lg-4">
+                                    <div class="col-lg-2">
+                                    <label>Codigo:<small class="text-muted" ></small></label>  
+                                    <?php 
+                                    ?>
+                                    <input type="text" class="form-control" id="fname"  name="codigo">                                     
+                                    </div>
+                                    
+                                    <div class="col-lg-3">
                                     <label>Nombre Comercial:<small class="text-muted" ></small></label>                                     
-                                    <input type="text" class="form-control" id="fname"  name="nombreCom" placeholder="Nombre Comercial del Insumo.">                                     
+                                    <input type="text" class="form-control" id="nombreCom"  name="nombreCom" placeholder="Nombre Comercial del Insumo.">                                     
                                     </div>
                                     
                                    
@@ -72,7 +92,7 @@ if (isset($_REQUEST['btnGuardar'])) {
                                     <input type="text" class="form-control" id="lname" name="marca" placeholder="Marca del Insumo.">                                     
                                     </div>
 
-                                    <div class="col-lg-3">
+                                    <div class="col-lg-4">
                                     <label>Descripción:<small class="text-muted" ></small></label>                                     
                                     <input type="text" class="form-control" id="lname" name="descripcion" placeholder="Descripción de Insumo.">                                     
                                     </div>
@@ -83,15 +103,15 @@ if (isset($_REQUEST['btnGuardar'])) {
                                     <input type="number" min="0" class="form-control" id="lname" name="precio" placeholder="Digite Precio">
                                     </div>
                                     
-                                  <div class="col-lg-4">
-                                     <label>Seleccione si el Insumo tiene caducidad:<small class="text-muted"></small></label>
+                                  <div class="col-lg-3">
+                                     <label>Seleccione si tiene caducidad:<small class="text-muted"></small></label>
                                      <select class="custom-select" name="tipoCaducidad" id="tipoCaducidad" style="width: 100%; height:36px;" >
                                          
-                                                <option value="Caducidad" selected>Tiene Caducidad</option>
-                                                <option value="No caducidad">No tiene Caducidad</option>
+                                                <option value="0" selected>Tiene Caducidad</option>
+                                                <option value="1">No tiene Caducidad</option>
                                         </select>
                                   </div>
-                                    <div class="col-lg-4">
+                                    <div class="col-lg-3">
                                  <label>Fecha de Caducidad:</label>
                                 <div class="input-group">
                                     <input type="text" class="form-control mydatepicker" name="fecha" id="fecha" placeholder="Ingrese fecha de Caducidad.">
@@ -117,7 +137,7 @@ if (isset($_REQUEST['btnGuardar'])) {
                                    
                                    <div class="col-lg-2">
                                     <label>Cantidad de Paquete:<small class="text-muted" ></small></label>                                     
-                                    <input type="number" class="form-control" id="lname" name="paquete" placeholder="Cantidad de Paquete." >
+                                    <input type="number" min="0" class="form-control" id="lname" name="paquete" placeholder="Cantidad de Paquete." >
                                     </div>
                                    
                                    <div class="col-lg-2">
@@ -133,16 +153,14 @@ if (isset($_REQUEST['btnGuardar'])) {
                                          <div class="row mb-12" style="float: right;margin-right: 20px; margin-top: 15px;">
                                              <button type="reset" class="btn btn-info" name="Cancelar" id="Cancelar">Cancelar </button>
                                          </div>
-                                      
-                                    </div> 
-                            </div>
-                                   
 
+                                     </div> 
+                                         </div>
+                                </div>
+                            </form>
                         </div>
-                        </form>
-                 </div>
 
-                    </div>
+                     </div>
                 
                   
 
@@ -199,7 +217,7 @@ $( function() {
 <script>
 $( function() {
     $("#tipoCaducidad").change( function() {
-        if ($(this).val() === "No caducidad") {
+        if ($(this).val() === "1") {
             $("#fecha").prop("disabled", true);
         } else {
             $("#fecha").prop("disabled", false);
@@ -208,6 +226,32 @@ $( function() {
 });
     </script>    
     
-    
+    <script src="http://code.jquery.com/jquery-1.0.4.js"></script>
+<script>
+      $(document).ready(function () {
+          $("#nombreCom").keyup(function () {
+              
+              var value = $(this).val();
+              $cod = value.substr(0,3);
+              if(value!=""){
+              var numero = Math.floor(Math.random() * (999-100))+100;
+              $codigo = $cod+numero;
+              $("#fname").val($codigo);
+          }else{
+              $("#fname").val("");
+          }
+              
+          });
+      });
+</script>
+
+ <script>
+        
+ function codigo()
+ {
+     
+    // var comercial = document.getElementById("nombreCom").valueOf()
+ }
+ </script>
     
      
