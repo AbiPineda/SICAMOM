afcb112a3882f375d8019c8f295334a8d4b23a95
Abigail <pineverdi@gmail.com>
2018-10-18 20:00:55 -0600

    revisar xD

--- Registros/comInsumo.php
+++ Registros/comInsumo.php
@@ -3,6 +3,7 @@
 include_once '../plantilla/cabecera.php';
 include_once '../plantilla/menu.php';
 include_once '../plantilla/menu_lateral.php';
+include_once '../Conexion/conexion.php';
     ?>
     <div class="page-wrapper" style="height: 671px;">
 
@@ -17,13 +18,18 @@ include_once '../plantilla/menu_lateral.php';
       
       <h2 class="heading--secondary">Registro de compra</h2>
       <form>
-
+          
         <div class="row mb-12"> 
 
         <div class="col-lg-3">
+            <?php
+            include_once '../Conexion/conexion.php';
+            $pro = mysqli_query($conexion, "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'clinica' AND TABLE_NAME = 't_compra'");
+            $row = $pro->fetch_array();
+            ?>
          <label style="color: black">Factura #<small class="text-muted" ></small></label>
           <div class="input-group">                         
-          <input type="text" class="form-control" id="Cpaquete" name="Cpaquete" value="" required>
+          <input type="text" class="form-control" id="factura" name="factura" value="<?php echo $row['AUTO_INCREMENT'] ?>" disabled>
          <div class="input-group-append">
       <span class="input-group-text"><i class="fas fa-ticket-alt"></i></span>
         </div> 
@@ -31,9 +37,12 @@ include_once '../plantilla/menu_lateral.php';
            </div>
 
        <div class="col-lg-4">
+           <?php
+           date_default_timezone_set('America/El_Salvador');
+           ?>
       <label style="color: black">Fecha</label>
         <div class="input-group">
-      <input type="text" class="form-control mydatepicker" name="fecha" id="fecha" placeholder="Ingrese">
+      <input type="text" class="form-control" aria-describedby="basic-addon1" id="FeActual" value="<?=date('d/m/y g:ia');?>" disabled>
        <div class="input-group-append">
       <span class="input-group-text"><i class="fa fa-calendar"></i></span>
        </div>
@@ -42,32 +51,44 @@ include_once '../plantilla/menu_lateral.php';
 
         <div class="col-lg-4">
          <label style="color: black">Proveedor<small class="text-muted" ></small></label>
-          <div class="input-group">                         
-          <input type="text" class="form-control" id="Cpaquete" name="Cpaquete" value="" required>
-         <div class="input-group-append">
-      <span class="input-group-text"><i class="fas fa-user"></i></span>
-        </div> 
-       </div>
+         <select class="custom-select" name="proveedor" id="proveedor">
+                                             <?php
+                                          include_once '../Conexion/conexion.php';
+                                          $pro= mysqli_query($conexion,"SELECT*from t_proveedor WHERE estado=1");
+                              ?>
+                            <option>Proveedor</option>
+                            <?php
+                             while ($row = mysqli_fetch_array($pro)) {
+                                         $prove=$row['id_proveedor'];
+                                           echo '<option value='."$row[0]".'>'.$row['1'].'</option>';
+                                    }
+                                    ?>
+                                        </select>
            </div>
 
-           <div class="col-lg-3">
+           <div class="col-lg-4">
          <label style="color: black">Insumo<small class="text-muted" ></small></label>
           <div class="input-group">                         
-          <input type="text" class="form-control" id="Cpaquete" name="Cpaquete" value="" required>
+          <input type="text" class="form-control" id="insumo" name="insumo">
          <div class="input-group-append">
       <span class="input-group-text"><i class="fas fa-ticket-alt"></i></span>
         </div> 
        </div>
            </div>
 
-      <div class="col-lg-4">
-      <label style="color: black">Fecha de Caducidad</label>
-        <div class="input-group">
-      <input type="text" class="form-control mydatepicker" name="fecha" id="fecha" placeholder="Ingrese">
-       <div class="input-group-append">
-      <span class="input-group-text"><i class="fa fa-calendar"></i></span>
-       </div>
-        </div>
+      <div class="col-lg-3">
+          <?php
+           date_default_timezone_set('America/El_Salvador');
+           ?>
+          <label style="color: black">Fecha de Caducidad<small class="text-muted"></small></label>
+          <div class="input-group">
+              <!--<input type="date" name="fecha" class="form-control mydatepicker" placeholder="Ingrese fecha de nacimiento">-->
+              <input type="date" name="user_date" class="form-control" id="user_date" max="2020-01-01" min="<?=date('d/m/y g:ia');?>" onChange="javascript:calcularEdad();"/>
+              <div class="input-group-append">
+                  <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+              </div>
+
+          </div>
         </div>
 
         <div class="col-lg-2">
@@ -109,7 +130,7 @@ include_once '../plantilla/menu_lateral.php';
     <div class="thumbnail__links">
       <ul class="list-inline m-b-0 text-center">
         
-        <table id="tabla" >
+        <table id="tablaCompra" >
                           <thead>
                             <tr>
                               <th>Código</th> 
@@ -137,53 +158,51 @@ include_once '../plantilla/menu_lateral.php';
                         </table>
       </ul>
     </div>
-    <div class="signup__overlay"></div>
   </div>
 
   <div class="container__child signup__form">
-   
-     <div class="scroll-window">
-  <table class="table table-striped table-hover user-list fixed-header">
+   <div class="wrap">
+              <script src="../html/js/jquery.min.js" ></script>
+            <script src="../html/js/buscaresc.js"></script>
+         <div class="search" center>
+            <input type="text" name="buscar" id="filtrar" class="searchTerm" placeholder="Que está buscando?">
+            <button type="submit" class="searchButton">
+              <i class="fa fa-search"></i>
+           </button>
+         </div>
+      </div>
+      
+     <div class="table-responsive">
+         <table class="table table-striped" id="tabla">
     <thead>
       <th><div>Código</div></th>
       <th><div>Nombre</div></th>
       
-      <th><div></div></th>
-    </thead>
-    <tbody>
-      <tr>
-        <td>AH754</td>
-        <td>Baja lengua</td>
-        
-      </tr>
+     <tbody class="buscar"> 
+     <br>
+    <?php
+        $sacar1 = mysqli_query($conexion, "SELECT * FROM t_insumo WHERE estado=1");
+            while ($fila = mysqli_fetch_array($sacar1)) {
+                $cod=$fila['codigo'];
+                $nom=$fila['ins_cnombre_comercial'];  
+                      
+        ?> 
+        <tr>
+                <td data-title="Relea"><?php echo $cod; ?></td>
+                <th scope="row"><?php echo $nom; ?></th>
+                
+                </td>
 
-      <tr>
-        <td>AH754</td>
-        <td>Gel antibacterial</td>
-        
-      </tr>
-
-      <tr>
-        <td>MY843</td>
-        <td>Guantes</td>
-        
-      </tr>
+       <?php  }?>
       
-    </tbody>
+      </tr>
+
+    </tbody>  
+    </thead>
+    
   </table>
   </div>
 
-  <div class="col-lg-12">
-          <div class="input-group">                         
-          <input type="text" id="search_input_all" onkeyup="FilterkeyWord_all_table()" placeholder="Buscar.." class="form-control">
-         <div class="input-group-append">
-      <span class="input-group-text"><i class="fas fa-search"></i></span>
-        </div> 
-       </div>
-           </div>
-      
-      
-  </div>
 </div>
 
           </div>
@@ -204,105 +223,15 @@ include_once '../plantilla/menu_lateral.php';
         document.f1.Tpagar.value =calculo;
                 }
                 </script>
-        <script type="text/javascript">
-            /**
-             * Funcion que devuelve true o false dependiendo de si la fecha es correcta.
-             * Tiene que recibir el dia, mes y año
-             */
-            function isValidDate(day, month, year)
+ 
+                <script>
+            var table = document.getElementById('tabla');
+            for(var i = 1; i<table.rows.length; i++)
             {
-                var dteDate;
-                month = month - 1;
-                dteDate = new Date(year, month, day);
-                return ((day == dteDate.getDate()) && (month == dteDate.getMonth()) && (year == dteDate.getFullYear()));
-            }
-            function validate_fecha(fecha)
-            {
-                var patron = new RegExp("^(19|20)+([0-9]{2})([-])([0-9]{1,2})([-])([0-9]{1,2})$");
-                if (fecha.search(patron) == 0)
+                table.rows[i].onclick = function()
                 {
-                    var values = fecha.split("-");
-                    if (isValidDate(values[2], values[1], values[0]))
-                    {
-                        return true;
-                    }
-                }
-                return false;
+                    document.getElementById("insumo").value = this.cells[1].innerHTML;
+               };
             }
-        </script>
-        <script>
-            $(function () {
-                $("#tipo").change(function () {
-                    if ($(this).val() === "No contable") {
-                        $("#unidad").prop("disabled", true);
-                    } else {
-                        $("#unidad").prop("disabled", false);
-                    }
-                });
-            });
-        </script>
-        <script>
-            $(function () {
-                $("#tipoCaducidad").change(function () {
-                    if ($(this).val() === "1") {
-                        $("#fecha").prop("disabled", true);
-                    } else {
-                        $("#fecha").prop("disabled", false);
-                    }
-                });
-            });
-        </script>    
-
-        <script src="http://code.jquery.com/jquery-1.0.4.js"></script>
-        <script>
-            $(document).ready(function () {
-                $("#nombreCom").keyup(function () {
-
-                    var value = $(this).val();
-                    $cod = value.substr(0, 3).toUpperCase();
-                    if (value != "") {
-                        var numero = Math.floor(Math.random() * (999 - 100)) + 100;
-                        $codigo = $cod + numero;
-                        $("#fname").val($codigo);
-                    } else {
-                        $("#fname").val("");
-                    }
-
-                });
-            });
-        </script>
-
-        <script>
-            function soloLetras(e) {
-                key = e.keyCode || e.which;
-                teclado = String.fromCharCode(key).toLowerCase();
-                letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
-                especiales = "8-37-38-46-164";
-                teclado_especial = false;
-                for (var i in especiales) {
-                    if (key == especiales[i]) {
-                        teclado_especial = true;
-                        break;
-                    }
-                }
-                if (letras.indexOf(teclado) == -1 && !teclado_especial) {
-                    return false;
-                }
-            }
-        </script>
-
-        <script>
-            function sinCaracterEspecial(e) {
-                tecla = (document.all) ? e.keyCode : e.which;
-                //Tecla de retroceso para borrar, siempre la permite
-                if (tecla == 8) {
-                    return true;
-                }
-                // Patron de entrada, en este caso solo acepta numeros, letras, espacio.
-                patron = /[A-Za-z0-9 ]/;
-                tecla_final = String.fromCharCode(tecla);
-                return patron.test(tecla_final);
-            }
-        </script>
-       
+            </script>
         
