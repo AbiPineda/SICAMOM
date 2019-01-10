cd8f70c41c951fc818944444b6eeb3e6425a7440
ticas7 <42784597+ticas7@users.noreply.github.com>
2019-01-10 13:48:38 -0600

    Update funcs.php
    inicio luego de entrar desde el login

--- Login/funcs/funcs.php
+++ Login/funcs/funcs.php
@@ -221,7 +221,7 @@
 					$_SESSION['id_usuario'] = $id;
 					$_SESSION['tipo_usuario'] = $id_tipo;
 					
-					header("location: welcome.php");
+					header("location: /SICAMOM/Html/ltr/index.html");
 					} else {
 					
 					$errors = "La contrase&ntilde;a es incorrecta";
