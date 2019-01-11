a5f9688c14ea38b9336b0bf0b83eb460ab0105bc
ticas7 <42784597+ticas7@users.noreply.github.com>
2019-01-10 23:37:27 -0600

    login
    login

--- Login/index.php
+++ Login/index.php
@@ -31,6 +31,8 @@
       -->
       <link href="../dist/css/styleLogin.css" rel="stylesheet">
 
+
+
 		<title>Login</title>
 		
 		<link rel="stylesheet" href="css/bootstrap.min.css" >
@@ -39,53 +41,73 @@
 		
 	</head>
 	
+
 	<body>
+
+
 		<div class="cont" style="background-image: url(../assets/images/ecografia.jpg);">
-		<div class="container">    
-			<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-				<div class="panel panel-info" >
-					<div class="panel-heading">
-						<div class="panel-title">Iniciar Sesi&oacute;n</div>
-						<div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="recupera.php">¿Se te olvid&oacute; tu contraseña?</a></div>
-					</div>     
+			<br> </br>
+		<div class="container"> 
+		 <div class="demo" >   
+			<div class="login">     
+			<div class="login__check">
+        		<img src="../assets/images/SICAMOM.png">
+      		</div> 
+      		<form id="loginform" class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off">             
+				<div class="login__form">
+					<div class="login__row">
+						<svg class="login__icon pass svg-icon" viewBox="0 0 20 20">
+           				 <path d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0" />
+          				</svg>
+						<!--<div class="panel-title">Iniciar Sesi&oacute;n</div>
+					<div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="recupera.php">¿Se te olvid&oacute; tu contraseña?</a></div>
+						-->
+						<input id="usuario" type="text" class="login__input name" placeholder="Usuario o email" name="usuario" autocomplete="off" required />
+					</div> 
+
+					<div class="login__row">
+          				<svg class="login__icon pass svg-icon" viewBox="0 0 20 20">
+            				<path d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0" />
+         			 	</svg>
+         				 <input  id= "password "type="password" class="login__input pass" placeholder="Contraseña" name="password" autocomplete="off"/>
+        			</div>    
 					
-					<div style="padding-top:30px" class="panel-body" >
+					<div style="padding-top:10px" class="panel-body" >
 						
 						<div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
 						
-						<form id="loginform" class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off">
-							<div class="login__check">
-        <img src="../assets/images/SICAMOM.png">
-      </div>
-							<div style="margin-bottom: 25px" class="input-group">
-								<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-								<input id="usuario" type="text" class="form-control" name="usuario" value="" placeholder="usuario o email" required>                                        
-							</div>
+						
 							
-							<div style="margin-bottom: 25px" class="input-group">
-								<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-								<input id="password" type="password" class="form-control" name="password" placeholder="password" required>
-							</div>
+						
 							
-							<div style="margin-top:10px" class="form-group">
-								<div class="col-sm-12 controls">
-									<button id="btn-login" type="submit" class="btn btn-success">Iniciar Sesi&oacute;n</a>
+							<div style="margin-top:-40px" class="form-group">
+								<div class=""login__submit"">
+									 <button type="submit" class="login__submit" name="Submit">Iniciar Sesión</button>
 								</div>
+
+								<div style="float:center; font-size: 90%; position: relative; top:-5px"><a href="recupera.php">¿Se te olvid&oacute; tu contraseña?</a></div>
 							</div>
+								<form id="loginform" class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off">
 							
+							<!--
 							<div class="form-group">
 								<div class="col-md-12 control">
 									<div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
 										No tiene una cuenta! <a href="registro.php">Registrate aquí</a>
 									</div>
 								</div>
-							</div>    
+							</div>
+							-->   
+
+							
 						</form>
 						<?php echo resultBlock($errors); ?>
 					</div>                     
 				</div>  
 			</div>
 		</div>
+		</div>
+
 	</div>
 	</body>
 </html>
