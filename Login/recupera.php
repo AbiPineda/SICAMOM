dbf00b48f764802db46a2d2f9e09386db1a172b0
ticas7 <42784597+ticas7@users.noreply.github.com>
2019-01-11 22:43:42 -0600

    Update recupera.php

--- Login/recupera.php
+++ Login/recupera.php
@@ -60,40 +60,41 @@
 	
 	<body>
 		<div class="cont" style="background-image: url(../assets/images/ecografia.jpg);">
-		<div class="container">    
-			<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-				<div class="panel panel-info" >
+		<div class="demo" >   
+			<div class="login">                    
+				 <div class="login__check">
+				 	<img src="../assets/images/SICAMOM.png">
+                 </div>
+                 <div class="login__form">
 					<div class="panel-heading">
-						<div class="panel-title">Recuperar Password</div>
-						<div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="index.php">Iniciar Sesi&oacute;n</a></div>
-					</div>     
-					
-					<div style="padding-top:30px" class="panel-body" >
+						<div class="panel-title" >Recuperar Password</div>
+					</div>     					
+					<div style="padding-top:-10px" class="panel-body" >
 						
 						<div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
 						
 						<form id="loginform" class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" autocomplete="off">
-							
-							<div style="margin-bottom: 25px" class="input-group">
-								<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-								<input id="email" type="email" class="form-control" name="email" placeholder="email" required>                                        
-							</div>
-							
-							<div style="margin-top:10px" class="form-group">
+							<div class="login__form">
+
+							<div class="login__row">
+          						<svg class="login__icon name svg-icon" viewBox="0 0 20 20">
+           						 <path d="M0,20 a10,8 0 0,1 20,0z M10,0 a4,4 0 0,1 0,8 a4,4 0 0,1 0,-8" />
+          						</svg>
+         							 <input id="email "type="email" class="login__input name" placeholder="email" name="email" required=/>
+        					</div>
+
+							<div style="margin-top:5px" class="form-group">
 								<div class="col-sm-12 controls">
-									<button id="btn-login" type="submit" class="btn btn-success">Enviar</a>
+										<button id="btn-login" type="submit" class="login__submit" name="Submit">Enviar</button>
+										<div style="float:center; font-size: 100%; position: relative; top:-10px"><a href="index.php">Iniciar Sesi&oacute;n</a></div>
 								</div>
 							</div>
 							
-							<div class="form-group">
-								<div class="col-md-12 control">
-									<div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-										No tiene una cuenta! <a href="registro.php">Registrate aquí</a>
-									</div>
-								</div>
-							</div>    
+							
+							</div>  
+							 </div> 
 						</form>
-					</div>                     
+					    </div>                  
 				</div>  
 			</div>
 		</div>
