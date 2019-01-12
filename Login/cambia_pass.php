8c5393ed4972142826e5a8a47f769a59e74d80bb
ticas7 <42784597+ticas7@users.noreply.github.com>
2019-01-11 23:28:08 -0600

    Update cambia_pass.php

--- Login/cambia_pass.php
+++ Login/cambia_pass.php
@@ -40,9 +40,12 @@ exit;
 	
 	<body>
 		<div class="cont" style="background-image: url(../assets/images/ecografia.jpg);">
-		<div class="container">    
-		<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-		<div class="panel panel-info" >
+		<div class="demo" >  
+		<div class="login">                      
+		<div class="login__check">
+				 	<img src="../assets/images/SICAMOM.png">
+                 </div>
+         <div class="login__form">
 			<div class="panel-heading">
 				<div class="panel-title">Cambiar Password</div>
 				<div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="index.php">Iniciar Sesi&oacute;n</a></div>
@@ -56,25 +59,28 @@ exit;
 					
 					<input type="hidden" id="token" name="token" value ="<?php echo $token; ?>" />
 					
-					<div class="form-group">
-						<label for="password" class="col-md-3 control-label">Nuevo Password</label>
-						<div class="col-md-9">
-							<input type="password" class="form-control" name="password" placeholder="Password" required>
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<label for="con_password" class="col-md-3 control-label">Confirmar Password</label>
-						<div class="col-md-9">
-							<input type="password" class="form-control" name="con_password" placeholder="Confirmar Password" required>
-						</div>
-					</div>
+					<div class="login__form">
+					<div class="login__row">
+          				<svg class="login__icon pass svg-icon" viewBox="0 0 20 20">
+           				 <path d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0" />
+          				</svg>
+          					<input type="password" class="login__input pass" placeholder="Nueva contrasena" name="password" required />
+        			</div>
+
+					<div class="login__row">
+          				<svg class="login__icon pass svg-icon" viewBox="0 0 20 20">
+           				 <path d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0" />
+          				</svg>
+          					<input type="password" class="login__input pass" placeholder="Confirmar contrasena" name="con_password" required />
+        			</div>
 					
 					<div style="margin-top:10px" class="form-group">
 						<div class="col-sm-12 controls">
 							<button id="btn-login" type="submit" class="btn btn-success">Modificar</a>
 						</div>
-					</div>   
+					</div>  
+					</div> 
+					</div> 
 				</form>
 			</div>                     
 		</div>  
