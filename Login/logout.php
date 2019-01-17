13d94e3b931498dbbd8839b0f07f3aa246ad6608
ticas7 <42784597+ticas7@users.noreply.github.com>
2019-01-16 21:11:29 -0600

    Redireccion

--- Login/logout.php
+++ Login/logout.php
@@ -2,5 +2,5 @@
     session_start(); //Iniciar una nueva sesión o reanudar la existente
     session_destroy(); //Destruye la sesión
   
-    header('location: /SICAMOM/SICAMOM/Html/ltr/index.html'); //Redirecciona la inicio
+    header('location: /SICAMOM/Login/index.php'); //Redirecciona la inicio
 ?>
