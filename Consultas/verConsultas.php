1eb635182d1a08b428541e1a6156b15a03b5f5bf
Abigail <pineverdi@gmail.com>
2019-01-20 22:10:50 -0600

    Revert "cambio"
    This reverts commit 468a353bc12d3b4a6a18cd8f6a60d96a26ca1358.

--- Consultas/verConsultas.php
+++ Consultas/verConsultas.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 include_once '../plantilla/cabecera.php';
 include_once '../plantilla/menu.php';
 include_once '../plantilla/menu_lateral.php';
@@ -143,21 +142,18 @@ t_paciente WHERE t_paciente.id_paciente='$modi'");
                                       <?php
                                       include_once '../Conexion/conexion.php';
         $sacar1 = mysqli_query($conexion, "SELECT
-t_expediente.id_expediente,
-t_paciente.pac_cnombre,
-t_paciente.pac_capellidos,
-t_medico.med_cnombre,
-t_consulta.con_receta,
-t_consulta.con_diagnostico,
-t_consulta.con_fecha_atiende,
 t_expediente.codigo,
-t_expediente.alergias
+t_expediente.alergias,
+t_consulta.con_fecha_atiende,
+t_consulta.con_diagnostico,
+usuarios.id
 FROM
 t_expediente
 INNER JOIN t_paciente ON t_expediente.fk_paciente = t_paciente.id_paciente
-INNER JOIN t_medico ON t_expediente.fk_medico = t_medico.idMedico
 INNER JOIN t_consulta ON t_consulta.fk_expediente = t_expediente.id_expediente
-WHERE t_expediente.id_expediente=$modi");
+INNER JOIN t_medico ON t_expediente.fk_medico = t_medico.idMedico
+INNER JOIN usuarios ON t_medico.fk_usuario = usuarios.id
+where usuarios.id=$modi");
             while ($fila = mysqli_fetch_array($sacar1)) {
              
                  $tipo=$fila['codigo'];  
